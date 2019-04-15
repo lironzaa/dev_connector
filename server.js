7735c1c41a0509de5d3d65712b36d0ed0145a680
lironzaa <lironzaa@gmail>
2019-04-16 01:02:37 +0300

    final deploy 3

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const port = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server runnning on port  ${port}`));
+app.listen(PORT);
